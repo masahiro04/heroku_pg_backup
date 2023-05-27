14623a6e69c216e96b0163faee2c1c74bff1558d
masahirookubo <stmhamachiii@gmail.com>
2023-05-27 14:43:18 +0900

    feat: implemented download file func

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
+use std::io::Error;
+use std::process::Command;
+use std::{env, panic};
 
-use std::env;
-use std::io::{Error, Write};
-use std::process::{Command, Stdio};
+use std::fs::File;
+use std::io::copy;
 
-fn download_latest_backup(app_name: &str, api_key: &str) -> Result<(), Error> {
-    env::set_var("HEROKU_API_KEY", api_key);
+use reqwest::blocking::get;
 
-    // Herokuのpg:backups:urlコマンドを実行してダウンロードURLを取得
+fn fetch_latest_s3_backup_url(app_name: &str) -> Result<String, Error> {
     let output = match Command::new("heroku")
         .arg("pg:backups:url")
         .arg("--app")
@@ -19,33 +17,33 @@ fn download_latest_backup(app_name: &str, api_key: &str) -> Result<(), Error> {
         Ok(o) => o,
         Err(e) => panic!("failed to execute process: {}", e),
     };
-    println!("output: {:?}", output);
-    //
-    // // コマンドの出力をダウンロードURLとして取得
-    // let download_url = match String::from_utf8(output.stdout) {
-    //     Ok(s) => s,
-    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    // };
-    // .unwrap().trim().to_string();
-    // println!("download_url: {}", download_url);
+    let download_url = match String::from_utf8(output.stdout) {
+        Ok(s) => s,
+        Err(e) => panic!("failed to execute process: {}", e),
+    };
+    let trimed_url = download_url.trim().to_string();
+    println!("trimed_url: {}", trimed_url);
 
-    // // curlコマンドを使用してダウンロード
-    // Command::new("curl")
-    //     .arg("-o")
-    //     .arg("latest.dump")
-    //     .arg(download_url)
-    //     .spawn()?;
+    Ok(trimed_url)
+}
 
+fn download_backup_file(url: &str) -> Result<(), Error> {
+    let mut file = File::create("backup.dump").expect("Failed to create file");
+    let mut response = get(url).expect("request failed");
+    copy(&mut response, &mut file).expect("copy failed");
     Ok(())
 }
 
 fn main() {
     let api_key = env::var("HEROKU_API_KEY").unwrap_or_else(|_| panic!("HEROKU_API_KEY not found"));
+    env::set_var("HEROKU_API_KEY", api_key);
+
     let app_name =
         env::var("HEROKU_APP_NAME").unwrap_or_else(|_| panic!("HEROKU_APP_NAME not found"));
 
-    match download_latest_backup(&app_name, &api_key) {
-        Ok(()) => println!("Download complete!"),
-        Err(e) => eprintln!("An error occurred: {}", e),
-    }
+    let download_url = match fetch_latest_s3_backup_url(&app_name) {
+        Ok(url) => url,
+        Err(e) => panic!("failed to fetch latest backup url: {}", e),
+    };
+    download_backup_file(&download_url).unwrap();
 }
